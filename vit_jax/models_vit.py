c4d447306ac950c6a78d25dcbf79af93d9e08abd
przeros <s180150@student.pg.edu.pl>
2024-05-15 18:20:36 +0200

    move MBBlocks before first NormLayer

--- vit_jax/models_vit.py
+++ vit_jax/models_vit.py
@@ -135,7 +135,9 @@ class Encoder1DBlock(nn.Module):
 
     # Attention block.
     assert inputs.ndim == 3, f'Expected (batch, seq, hidden) got {inputs.shape}'
-    x = nn.LayerNorm(dtype=self.dtype)(inputs)
+    x = MBConv(inp=hidden_size, oup=hidden_size, stride=1, expand_ratio=1.0, use_se=True, dtype=self.dtype)(inputs)
+    x = MBConv(inp=hidden_size, oup=hidden_size, stride=1, expand_ratio=1.0, use_se=True, dtype=self.dtype)(x)
+    x = nn.LayerNorm(dtype=self.dtype)(x)
     x = nn.MultiHeadDotProductAttention(
         dtype=self.dtype,
         kernel_init=nn.initializers.xavier_uniform(),
@@ -149,8 +151,6 @@ class Encoder1DBlock(nn.Module):
 
     # MLP block.
     y = nn.LayerNorm(dtype=self.dtype)(x)
-    y = MBConv(inp=hidden_size, oup=hidden_size, stride=1, expand_ratio=1.0, use_se=True, dtype=self.dtype)(y)
-    y = MBConv(inp=hidden_size, oup=hidden_size, stride=1, expand_ratio=1.0, use_se=True, dtype=self.dtype)(y)
     y = MlpBlock(
         mlp_dim=self.mlp_dim, dtype=self.dtype, dropout_rate=self.dropout_rate)(
             y, deterministic=deterministic)
